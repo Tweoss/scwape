d3cfa91ee3193419f4ced2fcb5bb67e9d6269e90
Francis Chua <francis@opennetworking.org>
2021-07-30 13:03:30 -0700

    bash and fish completion

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use clap_generate::{generators::Fish, generate_to};
+use clap_generate::{generators::{Fish, Bash}, generate_to};
 use clap::{IntoApp};
 
 include!("src/cli.rs");
@@ -7,7 +7,8 @@ fn main() {
     let mut app = Opts::into_app();
     app.set_bin_name("scwape");
 
-    let outdir = env!("CARGO_MANIFEST_DIR");
-    generate_to::<Fish, _, _>(&mut app, "scwape", outdir);
+    let outdir = env!("CARGO_MANIFEST_DIR").to_string() + "/completions";
+    generate_to::<Fish, _, _>(&mut app, "scwape", &outdir);
+    generate_to::<Bash, _, _>(&mut app, "scwape", &outdir);
 }
 
